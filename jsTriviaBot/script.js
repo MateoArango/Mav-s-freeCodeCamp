105c94e3557c5843102882d00419d9d9f980603a
MateoArango <mateoarango4533865@gmail.com>
2026-06-23 09:24:21 -0500

    feat: add initial trivia bot script with coding facts

---
+++ jsTriviaBot/script.js
@@ -0,0 +1,20 @@
+//steps 1-4
+console.log("Hello! I'm your coding fun fact guide!");
+
+let botName = 'Isa';
+let botLocation = 'Cocabana';
+let favoriteLanguage = 'ingles';
+
+console.log(`My name is ${botName} and I live on ${botLocation}.`);
+console.log(`My favorite programming language is ${favoriteLanguage}.`);
+//steps 5-6
+let codingFact = `I like python snake and ${favoriteLanguage}  language`;
+console.log(codingFact);
+//steps 7-8
+codingFact = "As well I love the snakes, you know buddy! although this is my favorite language: " + favoriteLanguage + ".";
+console.log(codingFact);
+//steps 9-10
+codingFact = `it's easy to learn with Duo: ${favoriteLanguage}`;
+console.log(codingFact);
+//step 11
+console.log(`It was fun sharing these facts with you. Goodbye! - ${botName} from ${botLocation}.`);
