5d813dcef8dd45c9e090a4aa66a46f7ff9a3a37c
MateoArango <mateoarango4533865@gmail.com>
2026-06-23 11:09:10 -0500

    feat: sentence maker

---
+++ sentenceMaker/script.js
@@ -0,0 +1,24 @@
+// step 1-2
+let adjective = "cold";
+let noun = "dog";
+let verb = "walk";
+let place = "Montañita";
+let adjective2 = "kind";
+let noun2 = "cat";
+// step 3-4
+let firstStory =
+  `Once upon a time, there was a(n) ${adjective} ${noun} who loved to eat ${noun2}. The ${noun} lived in a ${place} and had ${adjective2} nostrils that blew fire when it was ${verb}.`;
+// step 5
+console.log(`First story: ${firstStory}`);
+//step 6
+adjective = "hot";
+noun = "horse";
+verb = "sleep";
+place = "Cali";
+adjective2 = "cool";
+noun2 = "lion";
+//step 7 - 8
+let secondStory =
+  `Once upon a time, there was a(n) ${adjective} ${noun} who loved to eat ${noun2}. The ${noun} lived in a ${place} and had ${adjective2} nostrils that blew fire when it was ${verb}.`;
+//step 9
+  console.log(`Second story: ${secondStory}`);
